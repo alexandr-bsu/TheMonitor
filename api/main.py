daf940d4bb103a067ee282a93364f82dd2419013
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-01-27 14:59:57 +0300

    upd

--- api/main.py
+++ api/main.py
@@ -1,20 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from chat.schema.ChatSchema import ChatUser, ChatEntity
-
+from typing import Annotated
 import uvicorn
 from aiogram_dialog import StartMode, ShowMode
 from bot.main import bot
 from bot.fsm.chat import ChatSG
 from bot.main import bg_manager
+import json
 
 app = FastAPI()
 
 @app.post('/sendLogsMessage')
-async def send_logs_entity(chat: ChatEntity, transmitter_id: int):
+async def send_logs_entity(telegram_id:Annotated[int, Body()], message: Annotated[str,  Body()], transmitter_id: int):
     manager = bg_manager.bg(bot, transmitter_id, transmitter_id, load=True)
     await manager.start(ChatSG.sendMessage, mode=StartMode.RESET_STACK, show_mode=ShowMode.SEND,
-                        data={'chat': chat})
+                         data={'telegram_id': telegram_id, 'message': message})
 
-    return {'data': chat}
+    return {'data':
+        {'telegram_id': telegram_id, 'message': message}
+    }
 
-uvicorn.run(app, host='0.0.0.0', port=8080)
+uvicorn.run(app, port=8080)
--- bot/dialogs/sendChatEntity.py
+++ bot/dialogs/sendChatEntity.py
@@ -5,15 +5,16 @@ from aiogram_dialog.widgets.text import Format, Const, Case
 from aiogram_dialog import Dialog, DialogManager
 from bot.fsm.chat import ChatSG
 
+from datetime import datetime
+
 async def on_dialog_start(start_data: Any, manager: DialogManager):
-    chat = start_data['chat']
-    manager.dialog_data['sender'] = chat.sender
-    manager.dialog_data['message'] = chat.message
-    manager.dialog_data['timestamp'] = chat.timestamp.strftime('%d.%m.%Y %H:%M')
+    manager.dialog_data['telegram_id'] = start_data['telegram_id']
+    manager.dialog_data['message'] = start_data['message']
+    manager.dialog_data['timestamp'] = datetime.now().strftime('%d.%m.%Y %H:%M')
 
 message_received_window = Window(
-    Format('Пришло новое сообщение [{dialog_data[sender].role}]:\n'
-           'От кого: {dialog_data[sender].name} ({dialog_data[sender].id})\n'
+    Format(
+           'От кого: {dialog_data[telegram_id]}\n'
            'Когда: {dialog_data[timestamp]}\n\n'
            'Сообщение: {dialog_data[message]}'),
     state=ChatSG.sendMessage
